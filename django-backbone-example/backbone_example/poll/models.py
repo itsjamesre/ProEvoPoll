1d0c91917f3fbf11f14abc70b60289f9cb2dea42
James Re <jamie@projectevolution.com>
2012-04-11 13:36:25 -0400

    Simple model fix

--- django-backbone-example/backbone_example/poll/models.py
+++ django-backbone-example/backbone_example/poll/models.py
@@ -26,8 +26,6 @@ class Poll(models.Model):
 class Response(models.Model):
     poll = models.ForeignKey(Poll, related_name='responses')
 
-    title = models.CharField(("Title"), max_length=255, default='Response for ' + poll.title)
-
     rating_choice_1 = models.PositiveSmallIntegerField(("Rating Choice One"))
     rating_choice_2 = models.PositiveSmallIntegerField(("Rating Choice Two"))
     rating_choice_3 = models.PositiveSmallIntegerField(("Rating Choice Three"))
@@ -36,6 +34,3 @@ class Response(models.Model):
     multiple_choice = models.CharField(("Multiple Choice Answer"), max_length=255)
 
     essay_answer = models.CharField(("Essay Answer"), max_length=255)
-
-    def __unicode__(self):
-        return self.title
