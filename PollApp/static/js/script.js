e55b6dc732775b800d5e32144c5bc71dfa0f48e3
James Re <jamie@projectevolution.com>
2012-04-23 15:43:11 -0400

    trying to get scrolling to work

--- PollApp/static/js/script.js
+++ PollApp/static/js/script.js
@@ -22,69 +22,76 @@ $(document).ready(function(){
       if (window.orientation == 90 || window.orientation == -90) {   // Landscape
          poll_positions = l_poll_positions;
          scrollToPoll = poll_positions[dex];
-         $('#polls').animate({'margin-top': -scrollToPoll}, 200, 'easeOutQuad');
+         $('body,html').animate({scrollTop: scrollToPoll}, 200);
       } else {                                                       // Portrait
          poll_positions = p_poll_positions;
          scrollToPoll = poll_positions[dex];
-         $('#polls').animate({'margin-top': -scrollToPoll}, 200, 'easeOutQuad');
+      $('body,html').animate({scrollTop: scrollToPoll}, 200);
       }
    }
 
-   var obj = document.getElementById('polls');
-   obj.addEventListener('touchstart', function(event) {
-      t_startX = (event.touches[0].clientX);
-      t_startY = (event.touches[0].clientY);
-      m_start = $('#polls').cssNumber('margin-top');
-      event.preventDefault();
-   }, false);
+   var obj = document;
 
    obj.addEventListener('touchmove', function(event) {
-      t_endX = (event.touches[0].clientX);
-      t_endY = (event.touches[0].clientY);
-      var new_margin = -((t_startY-t_endY) - m_start);
-      if (new_margin<=0 && Math.abs(new_margin)<=poll_positions[poll_positions.length-1]) {
-         $('#polls').css('margin-top', new_margin+'px');
-      }
-      event.preventDefault();
-   }, false);
-   
-   obj.addEventListener('touchend', function(event) {
-      var m_end = Math.abs($('#polls').cssNumber('margin-top'));
-      poll_positions.push(m_end);
-      poll_positions.sort(function(a,b){return a-b;});
-      var dex = poll_positions.indexOf(m_end), val=0;
-      if ( (poll_positions[dex] - poll_positions[dex+1]) > (poll_positions[dex-1] - poll_positions[dex])) {
-         val = poll_positions[dex+1];
-      } else {
-         val = poll_positions[dex-1];
-      }
-      poll_positions.splice(poll_positions.indexOf(m_end),1);
-      dex = poll_positions.indexOf(val);
-      if (dex<0) { dex = 0; }
       $('.js-poll-select').removeClass('on');
-      $('.js-poll-select').eq(dex).addClass('on');
-      var scrollToPoll = poll_positions[dex];
-      $('#polls').animate({'margin-top': -scrollToPoll}, 500, 'easeOutQuad');
+   });
 
-      event.preventDefault();
+   obj.addEventListener('touchend', function(event) {
+      listenForPageY(window.pageYOffset, 'touchend', null);
    }, false);
+
+   var listenForPageY = function(start_y, action, index) {
+      var refresh = setInterval(function() {
+         var prev_y = start_y;
+         start_y = window.pageYOffset;
+         if (prev_y == start_y) {
+            if (action == 'touchend') {
+               var m_end = Math.abs(start_y);
+               poll_positions.push(m_end);
+               poll_positions.sort(function(a,b){return a-b;});
+               var dex = poll_positions.indexOf(m_end), val=0;
+               if ( (poll_positions[dex] - poll_positions[dex+1]) > (poll_positions[dex-1] - poll_positions[dex])) {
+                  val = poll_positions[dex+1];
+               } else {
+                  val = poll_positions[dex-1];
+               }
+               poll_positions.splice(poll_positions.indexOf(m_end),1);
+               dex = poll_positions.indexOf(val);
+               if (dex<0) { dex = 0; }
+               $('.js-poll-select').eq(dex).addClass('on');
+               var scrollToPoll = poll_positions[dex];
+               $('body,html').animate({scrollTop: scrollToPoll}, 200, 'easeInQuad');
+               clearInterval(refresh);
+            } else {
+               var scrollToPoll = poll_positions[index];`
+               // $('body,html').animate({scrollTop: scrollToPoll}, 400, 'easeInQuad');
+               $('body,html').scrollTop(scrollToPoll);
+               clearInterval(refresh);
+            }
+         }
+      }, 50);
+   };
+
+   $('.js-poll-select').live('click', (function() {
+      listenForPageY(window.pageYOffset, 'click', $(this).index());
+      return false;
+   }));
 
    /* === Button Click === */
 
-   $('.ratingStep fieldset a.radio').live('touchend', function() {
+   $('.ratingStep fieldset a.radio').live('click', function(e) {
       // apply "on" class
       $(this).siblings().removeClass('on');
       $(this).toggleClass('on');
-
       // set hidden input
       if ($(this).hasClass('on')) {
          $(this).parent().children('input').val($(this).text());
       }
-
-      return false;
+      $('#status').html('all click');
+      e.preventDefault();
    });
 
-   $('.multiStep fieldset a.radio').live('touchend', function() {
+   $('.multiStep fieldset a.radio').live('click', function() {
       // apply "on" class
       $(this).parent().parent().find('a.radio').removeClass('on');
       $(this).toggleClass('on');
@@ -97,25 +104,16 @@ $(document).ready(function(){
       return false;
    });
 
-   $('input[type=submit]').live('touchend',function() {
+   $('input[type=submit]').live('click',function() {
       $(this).click();
       return false;
    });
 
-   $('.js-poll-select').live('touchend',(function() {
-      $('.js-poll-select').removeClass('on');
-      $(this).addClass('on');
-      var scrollToPoll = poll_positions[$(this).index()];
-      console.log(scrollToPoll);
-      $('#polls').animate({'top': -scrollToPoll}, 1000, 'easeOutQuad');
-      event.preventDefault();
-   }));
-
-   $('textarea, input[type=text], input[type=email]').live('touchstart', function() {
+   $('textarea, input[type=text], input[type=email]').live('click', function() {
       $(this).focus();
    });
 
-   $('.opt_in, input[type=checkbox]').live('touchstart', function() {
+   $('.opt_in, input[type=checkbox]').live('click', function() {
       $(this).find('input[type=checkbox]').click();
       var opt_value = $(this).find('.user_opt').attr('value');
       if (opt_value === 'false') { $(this).find('.user_opt').attr('value', 'true'); }
